bd85a15dc32cbce6216b83a1113e8dcd0a4fd5d4
Iván Eduardo <eduardo.alba94@hotmail.com>
2021-07-13 23:25:43 -0500

    Se cambio titulo de la app

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Curso Git',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'GitHub'),
     );
   }
 }
