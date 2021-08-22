c870c0718b41f77b7dda7bd25f816ab159edcd43
Iván Eduardo <eduardo.alba94@hotmail.com>
2021-08-22 03:14:01 -0500

    se quito el nombre charal

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Curso Git and GitHub de charal',
+      title: 'Curso Git and GitHub',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
