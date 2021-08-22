8ad265aba2f74b1119e990ebda142a733a3c58f8
Iván Eduardo <31447148+spartanfenix@users.noreply.github.com>
2021-08-22 03:17:30 -0500

    Buen curso, palomita

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Apenas empieza lo bueno'),
+      home: MyHomePage(title: 'Buen curso, palomita'),
     );
   }
 }
